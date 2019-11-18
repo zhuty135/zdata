22096cb7010e5a39d72d62ac89dbff621809a9b3
zhuty135 <13564224337@163.com>
2019-11-18 18:12:49 +0800

    fix date index

--- csvpolish.py
+++ csvpolish.py
@@ -28,22 +28,30 @@ def fake_data(df,origin_fld='close'):
     df["adjusted"] = df[origin_fld]
     return df
 
-def fill_missing_data(fin,fout,index_fld):
+def fill_missing_data(fin,fout,index_col):
     df = None
     try:
-        df = pd.read_csv(fin,index_col=index_fld)
+        df = pd.read_csv(fin,index_col=index_col,parse_dates=True)
     except Exception as err:
         print(str(err))
-    df.columns = ['close']
-    sd = df.index[0]
-    ed = df.index[-1]
-    bd_list = get_business_date_list()
-    bd_list = bd_list[(bd_list >= sd) & (bd_list <= ed)]
-    df = df.reindex(bd_list,method='ffill')
-    df = fake_data(df)
+    dt_fmt='%Y%m%d'
+    df.index.names = ['date']
 
-    print(sd,ed,bd_list)
-    col_value=['open','high','low','close']
+    if df.shape[1] < 2:
+        df.columns = ['close']
+    
+    if df.empty:
+        return False
+    sd = df.index[0].strftime(dt_fmt)
+    ed = df.index[-1].strftime(dt_fmt)
+    bd_list = get_business_date_list(fmt=dt_fmt)
+    print(sd,ed,type(bd_list))
+    bd_list = pd.to_datetime(bd_list[(bd_list >= sd) & (bd_list <= ed)])
+    df = df.reindex(bd_list,method='ffill') 
+
+    if df.shape[1] < 2:
+        df = fake_data(df)
+
     df.sort_index().round(7).to_csv(fout, index=True,na_rep='')
     return True
 
@@ -69,9 +77,9 @@ def create_symb_link(d_type,i_type,ix_weight_file):
         hist_path = '/work/'+uname+'/input/' + d_type + '/sql/' + ex + '/daily/'
         forig = hist_path + i + '.csv'
         ffake = fake_path + i + '.csv'
-        fill_missing_data(forig,ffake,index_fld)
-        print(forig)
-        print(ffake)
+        print('forig',forig)
+        print('ffake',ffake)
+        fill_missing_data(forig,ffake,index_col)
         if filter_link_symb(i):
             fdst = dst_path + i + '.csv'
             print(fdst)
@@ -80,32 +88,35 @@ def create_symb_link(d_type,i_type,ix_weight_file):
             else:
                 print('symlink file exists, then skipped')
 
-def start_polish(input_dir,output_dir,index_fld):
+def start_polish(input_dir,output_dir,index_col):
     from os.path import isfile,join 
     files = [f for f in os.listdir(input_dir) if isfile(join(input_dir, f))]
+    print(input_dir,output_dir)
     print(files)
     os.makedirs(output_dir,exist_ok=True)
 
     for f in files:
         fin = input_dir + f
         fout = output_dir + f
-        fill_missing_data(fin,fout,index_fld)
+        print('fin',fin)
+        fill_missing_data(fin,fout,index_col)
+        print('fout',fout)
 
 
 def main():
     import getopt, sys
     try:
-        opts, args = getopt.getopt(sys.argv[1:],"i:k:o:v",["index_fld=","help"])
+        opts, args = getopt.getopt(sys.argv[1:],"i:k:o:v",["index_col=","help"])
     except getopt.GetoptError as err:
         print(str(err))
         usage()
         sys.exit(2)
     verbose = False
     root_dir = '/work/'+uname+'/'#os.getcwd()
-    key_str = 'Index'
-    input_dir  = root_dir + '/input/' + key_str + '/'
-    output_dir = root_dir + '/data/pol/'  + key_str + '/'
-    index_fld =  'date' 
+    key_str = None 
+    input_dir  = None
+    output_dir = None 
+    index_col =  'date' 
     for o, a in opts:
         if o == "-v":
             verbose = True
@@ -115,10 +126,24 @@ def main():
             key_str= a
         elif o == ('-o'):
             output_dir = a 
-        elif o == ('--index_fld'):
-            index_fld =  a 
-    output_dir = output_dir if re.match(r'.*\/.*', output_dir)  else output_dir + key_str + '/'
-    start_polish(input_dir,output_dir,index_fld)
+        elif o == ('--index_col'):
+            index_col =  a 
+    if input_dir is not None:
+        id_split = input_dir.split('/')
+        print('id_split', id_split[-1] )
+        
+        key_str = id_split[-3]+'/'+id_split[-2] if id_split[-1] == '' else id_split[-2]+'/'+id_split[-1] 
+        print(key_str)
+    elif key_str is not None:
+        input_dir  = root_dir + '/input/' + key_str + '/' 
+    else:
+        print('input_dir or key_str is missing')
+
+    input_dir = input_dir + '/' 
+    output_dir = root_dir + '/data/pol/'  + key_str + '/' 
+    print('output_dir',output_dir) 
+    
+    start_polish(input_dir,output_dir,index_col)
     assert(0)
 
     d_type='stock'
