9af43effa817f7e2cde24708e3980f4b5819b34e
zhuty135 <13564224337@163.com>
2020-12-03 12:52:50 +0800

    add a few prints

--- csvpolish.py
+++ csvpolish.py
@@ -46,12 +46,13 @@ def fill_missing_data(fin,fout,index_col,zfix):
     bd_list = get_business_date_list(fmt=dt_fmt)
     print(sd,ed,type(bd_list))
     short_bd_list = pd.to_datetime(bd_list[(bd_list >= sd) & (bd_list <= ed)])
-    print(df.index)
+    print('jzcheck', len(df.index),df.index)
     print(short_bd_list)
     try: 
         df = df.drop_duplicates()
     except Exception as err:
-        print(str(err))
+        print('jzerror:', str(err))
+    print('jzcheck2',len(df.index),df.index)
 
     df = df.reindex(short_bd_list,method='ffill')
     df = df.fillna(method='ffill') 
