e3dfe30812182ebcea5f9ffc2f3fe64c2fb5b2c1
zhuty135 <13564224337@163.com>
2021-01-26 16:35:08 +0800

    cfpa back to daily reb

--- runzdata.sh
+++ runzdata.sh
@@ -107,7 +107,7 @@ elif [ $d_type == 'zmpa' ]; then
     /work/jzhu/project/ql/script/zmpa.py -t tfpa -s 20160512 > /work/shared/daily/log/zmpa.tfpa.log 2>&1
     /work/jzhu/project/ql/script/zmpa.py -t cfca > /work/shared/daily/log/zmpa.cfca.log 2>&1
     /work/jzhu/project/ql/script/zmpa.py -t cfsa > /work/shared/daily/log/zmpa.cfsa.log 2>&1
-    /work/jzhu/project/ql/script/zmpa.py -t cfpa -r w > /work/shared/daily/log/zmpa.cfpa.log 2>&1
+    /work/jzhu/project/ql/script/zmpa.py -t cfpa > /work/shared/daily/log/zmpa.cfpa.log 2>&1
     scp -rp /work/shared/daily/ql/zmpa/* jzhu@106.14.226.83:/work/shared/daily/ql/zmpa/ 
     scp -rp /work/shared/daily/ql/zmpa/* jzhu@123.57.60.6:/work/dwhang/project/sit/Shiny/yzpa/
     
