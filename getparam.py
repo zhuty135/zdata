6dce2cfe8d5b1f1478c4276f0734c118f244c3d4
zhuty135 <13564224337@163.com>
2022-08-12 08:11:40 +0800

    TF

--- getparam.py
+++ getparam.py
@@ -13,10 +13,25 @@ from zutils import get_prev_business_date, get_business_date_list
 
 from os.path import isfile,join
 def file_filter(f):
-    if f[-4:] in ['.csv']:
+    if f[-4:] in ['.csv'] and not re.match(r'tmp/.*', f):
         return True
     else:
         return False
+
+def generate_key(t,maxkey,totaldf,srdict):
+    if re.match(r'.*\.\..*',maxkey):
+        plist = maxkey.split('.')
+        plist.remove('')
+        params = [ str(round(int(i)/10,1)) if i == '5' else i for i in plist]
+        paramstr = ','.join(params[2:])
+        
+    else:
+        paramstr = ','.join(maxkey.split('.')[2:])
+
+    isstr = ','.join([str(round(z,2)) for z in totaldf.loc[maxkey+'.is',] ] )
+    osstr = ','.join([str(round(z,2)) for z in totaldf.loc[maxkey+'.os',] ] )
+    finalstr = t + '=[' + paramstr +  ']' + '#' + t + ' ' + str(round(srdict[maxkey]/2,2)) + ' is ' + isstr + ';os ' +  osstr 
+    return(finalstr)
 
 def start_analysis(input_dir,output_dir,index_col,zfix):
     input_dir = '/work/jzhu/output/' + input_dir
@@ -26,7 +41,6 @@ def start_analysis(input_dir,output_dir,index_col,zfix):
     print(csvfiles)
     os.makedirs(output_dir,exist_ok=True)
 
-    tickerlist = [f.split('.')[0] for f in csvfiles] 
 
     totaldf = pd.DataFrame()
     
@@ -42,6 +56,10 @@ def start_analysis(input_dir,output_dir,index_col,zfix):
         else:
             print('File Szie is Zero:',fin)
     print('total',totaldf)
+
+    tickerlist = []
+    [ tickerlist.append('.'.join(f.split('.')[0:2])) for f in csvfiles if not '.'.join(f.split('.')[0:2]) in tickerlist] 
+    print(tickerlist)
     for t in tickerlist:
         srdict = {}
         tmpdf = totaldf[totaldf.index.str.contains(t)].sort_index()
@@ -55,12 +73,30 @@ def start_analysis(input_dir,output_dir,index_col,zfix):
                 srdict[mistr]  = tmpdf.loc[i,'sr']
 
         maxkey = max(srdict,key=lambda key: srdict[key])
-        paramstr = ','.join(maxkey.split('.')[2:])
-        isstr = ','.join([str(round(z,2)) for z in totaldf.loc[maxkey+'.is',] ] )
-        osstr = ','.join([str(round(z,2)) for z in totaldf.loc[maxkey+'.os',] ] )
-        finalstr = '[' + paramstr +  ']' + '#' + t + ' ' + str(round(srdict[maxkey]/2,2)) + ' is ' + isstr + ';os ' +  osstr 
-        print(finalstr)
+        fs = generate_key(t,maxkey,totaldf,srdict)
+        del srdict[maxkey]
+        print(fs)
 
+        maxkey1 = max(srdict,key=lambda key: srdict[key])
+        fs1 = generate_key(t,maxkey1,totaldf,srdict)
+        del srdict[maxkey1]
+        print(fs1)
+
+        maxkey2 = max(srdict,key=lambda key: srdict[key])
+        fs2 = generate_key(t,maxkey2,totaldf,srdict)
+        del srdict[maxkey2]
+        print(fs2)
+
+
+        maxkey3 = max(srdict,key=lambda key: srdict[key])
+        fs3 = generate_key(t,maxkey3,totaldf,srdict)
+        del srdict[maxkey3]
+        print(fs3)
+
+        maxkey4 = max(srdict,key=lambda key: srdict[key])
+        fs4 = generate_key(t,maxkey4,totaldf,srdict)
+        del srdict[maxkey4]
+        print(fs4)
 
     if False:
         fout = output_dir + f
